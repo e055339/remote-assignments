df67003481ccec7c8a622231609388fff48d0461
e055339 <e055339@gmail.com>
2024-04-01 16:56:08 +0800

    app.js: change jQuery to HTML DOM

--- Week-2/Assignment-4/app.js
+++ Week-2/Assignment-4/app.js
@@ -1,16 +1,34 @@
 //使用jQuery CDN
-let $bannerHead = $(".banner h1");
-$(".banner").on("click", function () {
-  if ($bannerHead.text() === "歡迎來到我的網站") {
-    $bannerHead.text("Have a Good Time!");
+// const $bannerHead = $(".banner h1");
+// $(".banner").on("click", function () {
+//   if ($bannerHead.text() === "歡迎來到我的網站") {
+//     $bannerHead.text("Have a Good Time!");
+//   } else {
+//     $bannerHead.text("歡迎來到我的網站");
+//   }
+// });
+
+// const $moreClass = $(".moreClass");
+// $moreClass.css("display", "none"); //或直接用hide();
+
+// $(".btn").on("click", function () {
+//   $moreClass.show();
+// });
+
+const banner = document.querySelector(".banner");
+const bannerHead = document.querySelector(".banner h1");
+banner.addEventListener("click", () => {
+  if (bannerHead.innerHTML === "歡迎來到我的網站") {
+    bannerHead.innerHTML = "Have a Good Time!";
   } else {
-    $bannerHead.text("歡迎來到我的網站");
+    bannerHead.innerHTML = "歡迎來到我的網站";
   }
 });
 
-let $moreClass = $(".moreClass");
-$moreClass.css("display", "none"); //或直接用hide();
+const moreClass = document.querySelector(".moreClass");
+moreClass.style.display = "none";
 
-$(".btn").on("click", function () {
-  $moreClass.show();
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", () => {
+  moreClass.style.display = "flex";
 });
